d067e1db4e0531594c97114be60643e7e80ec8b4
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2015-11-24 18:42:58 +0200

    Minor improvements to repo fetching
    * Import configuration settings from environment
    * Create organization folders

--- fetch-repos.pl
+++ fetch-repos.pl
@@ -5,21 +5,37 @@ use Net::GitHub;
 use Git::Repository;
 use Data::Dumper;
 
-my $debug = 0;
+my $debug = $ENV{'GITHUB_DEBUG'} || 0;
 # User belonging to the organization
-my $git_username = 'some_username';
-my $git_password = 'some_password';
+my $git_username = $ENV{'GITHUB_USER'} || die "GITHUB_USER environment variable not found";
+my $git_password = $ENV{'GITHUB_PASS'} || die "GITHUB_PAS environment variable not found";
+
+# Check if the given folder exists and create if it doesn't
+sub ensureFolder {
+	my $path = shift;
+	if (! -d $path) {
+		print "Creating folder $path\n" if $debug;
+		mkdir $path;
+	}
+}
+
+my $dest = $ENV{'GITHUB_DEST'} || '';
+if ($dest) {
+	ensureFolder($dest);
+}
 
 my $github = Net::GitHub->new( 'version' => 3, 'login' => $git_username, 'pass' => $git_password );
 my @orgs = $github->org->orgs;
 foreach my $org (@orgs) {
 	print 'Fetching repos for ' . $org->{'login'} . "\n" if $debug;
+	ensureFolder($dest . '/' . $org->{'login'});
 	my @repos = $github->repos->list_org($org->{'login'});
 	foreach my $repo (@repos) {
 		my $clone_url = $repo->{'clone_url'};
-		my $dest_dir = $clone_url; 
+		my $dest_dir = $clone_url;
 		$dest_dir =~ s#^.*/##;
 		$dest_dir =~ s#\.git$##;
+		$dest_dir = join('/', $dest, $org->{'login'}, $dest_dir); 
 
 		$clone_url =~ s#^https://#https://${git_username}:${git_password}@#i;
 		if (-e $dest_dir) {
@@ -29,7 +45,7 @@ foreach my $org (@orgs) {
 		}
 		else {
 			print "Cloning repo [$clone_url]\n" if $debug;
-			Git::Repository->run('clone' => $clone_url);
+			Git::Repository->run('clone' => $clone_url, $dest_dir);
 		}
 	}
 }
